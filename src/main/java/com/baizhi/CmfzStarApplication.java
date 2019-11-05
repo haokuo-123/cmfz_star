b78ea84f0bafe9446e65ea788ecd1b9ccf9c443a
haokuo-123 <57389718+haokuo-123@users.noreply.github.com>
2019-11-05 17:26:46 +0800

    Update CmfzStarApplication.java

--- src/main/java/com/baizhi/CmfzStarApplication.java
+++ src/main/java/com/baizhi/CmfzStarApplication.java
@@ -15,6 +15,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 public class CmfzStarApplication {
 
     public static void main(String[] args) {
+        //测试测试
 
         SpringApplication.run(CmfzStarApplication.class, args);
     }
