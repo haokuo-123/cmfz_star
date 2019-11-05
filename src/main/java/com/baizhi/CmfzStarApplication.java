4594ae23234bda4a327095143181bfbcdc77cf69
test <haok146@163.com>
2019-11-05 17:32:25 +0800

    update test

--- src/main/java/com/baizhi/CmfzStarApplication.java
+++ src/main/java/com/baizhi/CmfzStarApplication.java
@@ -16,6 +16,7 @@ public class CmfzStarApplication {
 
     public static void main(String[] args) {
         //测试测试
+        //测试2
 
         SpringApplication.run(CmfzStarApplication.class, args);
     }
